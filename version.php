209b972e3f0f51457b1eb1170ff9519beaedb78d
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 16:23:06 +0800

    Update version.php

--- version.php
+++ version.php
@@ -1,30 +1,8 @@
 <?php
 
 return [
-    '1.0.0' => [
-        'Initialize extension.',
-        '修复添加问题',
-    ],
-    '1.0.1' => [
-        'Initialize extension.',
-        '修复添加问题',
-    ],
-    '1.0.2' => [
-        'Initialize extension.',
-        '修复添加问题',
-        '在菜单页自定义按钮',
-    ],
-    '1.0.3' => [
-        'Initialize extension.',
-        '修复添加问题',
-        '在菜单页自定义按钮,fix',
-    ],
-     '1.0.4' => [
-        'Initialize extension.',
-        '修复包的引入',
-    ],
      '1.0.5' => [
         'Initialize extension.',
-        '添加按钮控制器',
+        '添加菜单权限同步按钮',
     ],
 ];
