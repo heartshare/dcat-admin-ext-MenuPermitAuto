2c34a8db1157d7f55d6f301af46b2c5e60d4cfee
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 16:52:43 +0800

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('menu-permit-auto', Controllers\MenuPermitAutoController::class.'@index');
 
-Route::get('sync-menu-permit', Controllers\MenuPermitAutoController::class.'@syncMenuPeimit');
+Route::get('auth/sync-menu-permit', Controllers\MenuPermitAutoController::class.'@syncMenuPeimit');
