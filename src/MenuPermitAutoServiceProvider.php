af0134d77fac37bb637f57336bc5d337a78aba7f
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 09:11:41 +0800

    Update MenuPermitAutoServiceProvider.php

--- src/MenuPermitAutoServiceProvider.php
+++ src/MenuPermitAutoServiceProvider.php
@@ -15,6 +15,26 @@ class MenuPermitAutoServiceProvider extends ServiceProvider
 		'css/index.css',
 	];*/
 
+public function boot()
+    {
+        Admin::navbar(function (Navbar $navbar) {
+            if (request()->is('admin/auth/menu')) {
+                Admin::script(<<<SCRIPT
+                    const cardHeader = document.querySelector('.card-header.pb-1.with-border');
+                    const newDiv = document.createElement('div');
+                    const button = document.createElement('button');
+                    const link = document.createElement('a');
+                    button.textContent = '菜单权限同步';
+                    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+                    link.appendChild(button);
+                    link.href = '/admin/auth/permissions';
+                    newDiv.appendChild(link);
+                    cardHeader.appendChild(newDiv);
+                SCRIPT);
+            }
+        });
+    }
+
     protected $middleware = [
         'middle' => [ // 注册中间件
             MenuPermitAuto::class,
