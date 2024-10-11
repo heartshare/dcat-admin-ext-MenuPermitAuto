c68d6ade39875dad297b7c831a55c6afa98b5028
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 17:42:20 +0800

    Update MenuPermitAutoController.php

--- src/Http/Controllers/MenuPermitAutoController.php
+++ src/Http/Controllers/MenuPermitAutoController.php
@@ -6,8 +6,18 @@ use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Admin;
 use Illuminate\Routing\Controller;
 
+use Dcat\Admin\Models\Menu;
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+use Dcat\Admin\Models\Permission;
+use Illuminate\Support\Facades\DB;
+
 class MenuPermitAutoController extends Controller
 {
+    protected $signature = 'admin:menu';
+    protected $description = 'backup or populate menu data';
+    private $backupPath = '';
+    
     public function index(Content $content)
     {
         return $content
@@ -17,6 +27,93 @@ class MenuPermitAutoController extends Controller
     }
 
     public function syncMenuPeimit() {
+        $this->backupPath = storage_path('menu-backup.bak');
+        $this->backupMenu();
+        $this->fillMenu();
+        
         return 200;
     }
+
+    /**
+     * backup menu
+     */
+    private function backupMenu()
+    {
+        $res = file_put_contents($this->backupPath, serialize(optional(Menu::get())->toArray()));
+
+        echo $res ? 'Backup succeeded' : 'Backup failed';
+    }
+    
+    /**
+     * fill menu
+     */
+    private function fillMenu()
+    {
+        $menu = unserialize(file_get_contents($this->backupPath));
+
+        $permission = $this->generatePermissions($menu);
+
+        Menu::truncate();
+        Menu::insert($menu);
+
+        Permission::truncate();
+        Permission::insert($permission);
+
+        DB::table('admin_permission_menu')->truncate();
+        foreach ($permission as $item) {
+            $query = DB::table('admin_permission_menu');
+            $query->insert([
+                'permission_id' => $item['id'],
+                'menu_id'       => $item['id'],
+            ]);
+            if ($item['parent_id'] != 0) {
+                $query->insert([
+                    'permission_id' => $item['id'],
+                    'menu_id'       => $item['parent_id'],
+                ]);
+            }
+        }
+
+        echo 'Filled successfully';
+    }
+
+    private function generatePermissions($menu)
+    {
+        $permissions = [];
+        foreach ($menu as $item) {
+            $temp = [];
+
+            $temp['id']         = $item['id'];
+            $temp['name']       = $item['title'];
+            $temp['slug']       = (string)Str::uuid();
+            $temp['http_path']  = $this->getHttpPath($item['uri']);
+            $temp['order']      = $item['order'];
+            $temp['parent_id']  = $item['parent_id'];
+            $temp['created_at'] = $item['created_at'];
+            $temp['updated_at'] = $item['updated_at'];
+
+            $permissions[] = $temp;
+            unset($temp);
+        }
+
+        return $permissions;
+    }
+
+    private function getHttpPath($uri)
+    {
+        if ($uri == '/') {
+            return '';
+        }
+
+        if ($uri == '') {
+            return '';
+        }
+
+        if (strpos($uri, '/') !== 0) {
+            $uri = '/' . $uri;
+        }
+
+        return $uri . '*';
+    }
+    
 }
