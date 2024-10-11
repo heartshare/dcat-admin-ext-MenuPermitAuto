cc879d6eb2654f6ca90c00dc8061e57e3af849b9
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 18:08:48 +0800

    Update MenuPermitAutoServiceProvider.php

--- src/MenuPermitAutoServiceProvider.php
+++ src/MenuPermitAutoServiceProvider.php
@@ -32,7 +32,7 @@ class MenuPermitAutoServiceProvider extends ServiceProvider
 		parent::init();
 
 		 Admin::navbar(function (Navbar $navbar) {
-            if (request()->is('admin/auth/menu')) {
+            	if (request()->is('admin/auth/menu')) {
                 Admin::script(<<<SCRIPT
                     const cardHeader = document.querySelector('.card-header.pb-1.with-border');
                     const newDiv = document.createElement('div');
@@ -40,29 +40,29 @@ class MenuPermitAutoServiceProvider extends ServiceProvider
                     button.textContent = '菜单权限同步';
                     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
                     button.addEventListener('click', () => {
-                        fetch('/admin/auth/sync-menu-permit')
-                            .then(response => {
-                                if (!response.ok) {
-                                    throw new Error('网络响应失败');
-                                    Dcat.error('服务器出现未知错误1');
-                                }
-                                return response.json();
-                            })
-                            .then(data => {
-			        if (data == 200) {
-			      	    Dcat.success("菜单权限同步成功!");
-			            setTimeout(function () {Dcat.reload();}, 1000);
-				} else {
-			      	     Dcat.error('服务器出现未知错误2');
-				}
-                            })
-                            .catch((error) => {
-                                 console.error('错误:', error);
-                                 Dcat.error('服务器出现未知错误3');
-                            });
-                    });
-                    newDiv.appendChild(button);
-                    cardHeader.appendChild(newDiv);
+                    fetch('/admin/auth/sync-menu-permit')
+                        .then(response => {
+                            if (!response.ok) {
+                                throw new Error('网络响应失败');
+                                Dcat.error('服务器出现未知错误1');
+                            }
+                            return response.json();
+                        })
+                        .then(data => {
+                            if (data == 200) {
+                                Dcat.success("菜单权限同步成功!");
+                                setTimeout(function () { Dcat.reload(); }, 1000);
+                            } else {
+                                Dcat.error('服务器出现未知错误2');
+                            }
+                        })
+                        .catch((error) => {
+                            console.error('错误:', error);
+                            Dcat.error('服务器出现未知错误3');
+                        });
+                });
+                newDiv.appendChild(button);
+                cardHeader.appendChild(newDiv);
                 SCRIPT);
             }
         });
