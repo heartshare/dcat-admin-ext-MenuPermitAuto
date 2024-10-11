c08f32cc75150338134ebfa73b7897fdd581861c
smaillTony <tony654cxxf@derdk.onmicrosoft.com>
2024-10-11 09:11:01 +0800

    Update version.php

--- version.php
+++ version.php
@@ -3,9 +3,15 @@
 return [
     '1.0.0' => [
         'Initialize extension.',
+        '修复添加问题',
     ],
     '1.0.1' => [
         'Initialize extension.',
         '修复添加问题',
+    ],
+    '1.0.2' => [
+        'Initialize extension.',
+        '修复添加问题',
+        '在菜单页自定义按钮',
     ],
 ];
